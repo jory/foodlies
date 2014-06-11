1953e71fc28eb38b6cb4d052329b6f74a8dcf0fe
Jory Graham <jory.graham@gmail.com>
2014-06-11 01:07:26 -0400

    Passport changes and I'm finally logged in!

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -4,7 +4,7 @@ angular.module('foodiesApp')
   .controller('LoginCtrl', ['$scope', 'Auth', function ($scope, Auth) {
     $scope.login = function() {
       Auth.login({
-        name: $scope.name
+        username: $scope.username
       });
     }
   }]);
--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -4,7 +4,7 @@ angular.module('foodiesApp')
   .controller('SignupCtrl', ['$scope', 'Auth', function ($scope, Auth) {
     $scope.signup = function() {
       Auth.signup({
-        name: $scope.name
+        username: $scope.username
       });
     }
   }]);
