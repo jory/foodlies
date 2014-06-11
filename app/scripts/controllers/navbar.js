ae5fb0c00ccce6b3853961c22bec823229d52f97
Jory Graham <jory.graham@gmail.com>
2014-06-11 01:32:16 -0400

    Show a logout link in the Navbar

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -4,13 +4,23 @@ angular.module('foodiesApp')
   .controller('NavbarCtrl', function ($scope, $location) {
     $scope.menu = [{
       'title': 'Home',
-      'link': '/'
+      'link': '/',
+      'show': function() { return true; }
     }, {
       'title': 'Signup',
-      'link': '/signup'
+      'link': '/signup',
+      'show': function() { return !$scope.currentUser; }
     }, {
       'title': 'Login',
-      'link': '/login'
+      'link': '/login',
+      'show': function() { return !$scope.currentUser; } 
+    },  {
+      'title': 'Logout',
+      'link': '/logout',
+      'show': function() {
+        var path = $location.path();
+        return $scope.currentUser && (path !== '/login' && path !== '/signup');
+      }
     }];
     
     $scope.isActive = function(route) {
